49bd088c8ec40dc3516e5b9657b7662c4a80b65f
minhha2k3 <hadep7a@gmail.com>
2023-11-23 10:18:18 +0700

    fix file booking.html

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.secret_key = '^%^&$^T&*Y(*&*^&*^*(&&*$^4765876986764^&%&*%^%$&*^(*^*%*&^436'
 app.config["SQLALCHEMY_DATABASE_URI"] = (
-        'mysql+pymysql://springstudent:springstudent@localhost/phongkhamtunhan?charset=utf8mb4')
+        'mysql+pymysql://springstudent:%s@localhost/phongkhamtunhan?charset=utf8mb4'%quote('Admin@123'))
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["SQLALCHEMY_RECORD_QUERIES"] = True
 
