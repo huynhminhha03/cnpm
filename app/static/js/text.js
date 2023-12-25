4c448cdeed37e69b0354241656bef9fb56e5adc2
darklord0710 <ducmanhnguyen0710@gmail.com>
2023-12-25 23:45:48 +0700

    update

--- app/static/js/text.js
+++ app/static/js/text.js
@@ -3,40 +3,36 @@ var dsLieuLuongThuoc = document.querySelector("#dsLieuLuongThuoc");
 var motLoaiThuoc = document.querySelector(".motLoaiThuoc");
 var array_motLoaiThuoc = document.querySelectorAll('.motLoaiThuoc')
 var array_deleteDivButton = document.querySelectorAll('.deleteDivButton')
+var array_deleteDiv = document.querySelectorAll('.deleteDiv')
+
+array_deleteDiv[0].classList.add('hide')
+
 createDivButton.addEventListener('click',function () {
-    addMotLoaiThuoc()
+
+    if(array_deleteDiv.length === 1) {
+        array_deleteDiv[0].classList.remove('hide')
+        addMotLoaiThuoc()
+        array_deleteDiv[0].classList.add('hide')
+    }else {
+        addMotLoaiThuoc()
+    }
 });
 
 function addMotLoaiThuoc() {
         // Tạo một phần tử div mới
-        var clonedDiv = motLoaiThuoc.cloneNode(true);
-        dsLieuLuongThuoc.appendChild(clonedDiv);
+        let clonedDiv = motLoaiThuoc.cloneNode(true);
+        dsLieuLuongThuoc.appendChild(clonedDiv)
         array_motLoaiThuoc = document.querySelectorAll('.motLoaiThuoc')
         dsLieuLuongThuoc = document.querySelector("#dsLieuLuongThuoc")
         array_deleteDivButton = document.querySelectorAll('.deleteDivButton')
+
+        for (let i = 0 ; i < array_deleteDivButton.length ; i++){
+        array_deleteDivButton[i].addEventListener('click',function () {
+             var elementToRemove = array_motLoaiThuoc[i]
+             elementToRemove.remove()
+        });
+   }
 }
 
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     // Lấy tất cả các phần tử có class "delete"
-//     array_deleteDivButton = dsLieuLuongThuoc.querySelectorAll('.deleteDivButton')
-//
-//     // Gắn sự kiện click cho mỗi nút "delete"
-//     array_deleteDivButton.forEach(function(button, index) {
-//       button.addEventListener('click', function() {
-//         // Thực hiện các hành động khi nút "delete" được click
-//         console.log('Clicked on delete button ' + (index + 1));
-//       });
-//     });
-//     dsLieuLuongThuoc = document.querySelector("#dsLieuLuongThuoc");
-//     array_deleteDivButton = dsLieuLuongThuoc.querySelectorAll('.deleteDivButton')
-//   });
-
-for (let i = 0; i < array_deleteDivButton.length; i++) {
-    array_deleteDivButton = document.querySelectorAll('.deleteDivButton')
-
-    array_deleteDivButton[i].onclick = function() {
-         console.log('Clicked on delete button ' + (i + 1));
-    }
-}
 
